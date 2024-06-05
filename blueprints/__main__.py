06f3e0919e563ce01ea8b3600b1a9f1291459afb
David <c0d3@gpobox.net>
2024-06-05 22:38:10 +1000

    Add script to run blueprints

--- blueprints/__main__.py
+++ blueprints/__main__.py
@@ -1,19 +1,3 @@
-from flask import Flask
-from blueprints import register_blueprints, parse_arguments
+from blueprints.main import run
 
-args = parse_arguments()
-app = Flask('blueprints')
-register_blueprints('blueprints.txt', app)
-
-context = {
-    'host': args.host,
-    'port': args.port,
-}
-
-try:
-    from waitress.server import create_server
-    print(f"Serving requests on http://{args.host}:{args.port}")
-    server = create_server(app, **context)
-    server.run()
-except ImportError:
-    app.run(**context)
+run()
